863338dfba5cdefdc8918458062c2355e3ddaa5f
leonardosilva <leonardo.silva@zallpy.com>
2022-10-06 18:41:03 -0300

    ajustes no tratamento de excessoes

--- projeto_pontos/src/controllers/clientController.js
+++ projeto_pontos/src/controllers/clientController.js
@@ -2,26 +2,29 @@ const {
     getClientById,
     getAllClients,
     createClient, 
-}= require('../services/clientServices')
+} = require('../services/clientServices')
 
 const getAll = async (req,res) =>{
     let clients = await getAllClients()
     !clients ? res.status(204).send(clients) : res.status(200).json(clients)
 }
 
-const getById = async (req,res) => {
+const getById = async (req,res,next) => {
     try{
-        let client = await getClientById(req.params.id)
-        client ? res.status(200).send(client) : res.status(204).send()
+       let clientRequest = await getClientById(req.params.id)
+       return res.status(200).json(clientRequest)
     }catch(err){
-        console.log(err)
+        next(err)
     }
 
 }
 const createClientController = (req,res,next) => {
-    let client = req.body
-    let clientCreated = createClient(client)
-    res.status(201).send(clientCreated)
+    try{
+        let clientCreated = createClient(req.body)
+        return res.status(200).json(clientCreated)
+    }catch(err){
+        next(err)
+    }
 }
 
 module.exports = { getAll, getById, createClientController }
--- projeto_pontos/src/services/clientServices.js
+++ projeto_pontos/src/services/clientServices.js
@@ -2,29 +2,24 @@ const client = require('../models/client')
 const ErrorResponse = require('../utils/errorResponse')
 
 const getClientById = async ( id ) => {
-    try{
-        return await client.findOne({ _id: id})
-    }catch(err){
-        new ErrorResponse("Not found", 404)
-    } 
+    let clientResponse = await client.findById(id)
+    if (!clientResponse){
+        throw new ErrorResponse("Usuário não existe", 404)
+    }
+    return clientResponse
 }
-
 const getAllClients = async () => {
-    try{
-        let clients = await client.find({})
-        return clients
-    }catch(err){
-        return err
-    }
+    return await client.find({})
 }
 
-const createClient = async (req,res,next) => {
-    try{
-        console.log(req.body.name)
-    }catch(err){
-        return err
-    }
-   // let clientCreated = await client.create(clientRequest)
-    //return clientCreated
+const createClient = async ( request ) => {
+  let clientRequest = request
+  console.log(clientRequest)
+//   if (clientRequest.name == 'leo'){
+//     throw new ErrorResponse("Not found", 400)
+//   }
+  let clientCreated = await client.create(clientRequest)
+
+  return clientCreated    
 }
  module.exports = { getClientById, getAllClients, createClient }
